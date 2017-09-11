295c3e61cf58d64eab48d49dd175e88f130902e8
Tim Croydon <timcroydon@users.noreply.github.com>
2017-09-11 13:52:56 +0100

    do minor code tidyup

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,5 @@
-// TODO: initialise array
-// TODO: shuffle array
-// TODO: populate array
-
 var SUITS = ['hearts', 'clubs', 'diamonds', 'spades']
 var VALUES = ['ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'jack', 'queen', 'king']
-
-var deck = Array.from(Array(52).keys())
 
 // fischer-yates shuffle shamelessly lifted from https://bost.ocks.org/mike/shuffle/
 function shuffle(array) {
@@ -30,8 +24,12 @@ function shuffle(array) {
     return copy;
 }
 
+// initialise an array [0, 1, 2, ... , 50, 51] (no jokers)
+var deck = Array.from(Array(52).keys())
+
 var shuffled = shuffle(deck)
 
+// map shuffled array of numbers to corresponding card names (e.g. 2_of_hearts.svg)
 shuffled = shuffled.map(function (card) {
     var suit = SUITS[Math.floor(card / 13)]
     var face = card % 13
@@ -40,7 +38,7 @@ shuffled = shuffled.map(function (card) {
 
     // TODO: make picture cards optional
     if (face >= 10) {
-        filename += '2'
+        filename += '2' // 2 implies proper face cards
     }
 
     return filename + '.svg'
@@ -48,6 +46,7 @@ shuffled = shuffled.map(function (card) {
 
 vm = {
     // TODO: make number of card configurable
+    // take first 9 cards
     cards: shuffled.slice(0, 9)
 }
 
